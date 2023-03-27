e84af87424b1383ecff1ae23d7d742a9d6a6de36
ffati <alnaserfatimah344@gmail.com>
2023-03-27 12:20:04 +0300

    new edit

--- app/src/main/java/com/example/labapp/SecondActivity.java
+++ app/src/main/java/com/example/labapp/SecondActivity.java
@@ -2,13 +2,19 @@ package com.example.labapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-
+    TextView receiver_msg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        receiver_msg = (TextView) findViewById(R.id.received_value_id);
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("PersonName");
+        receiver_msg.setText("Hello, "+str+" !");
     }
 }
